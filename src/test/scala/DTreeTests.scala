bf1dbaaf097fe0b24efad5030941e5849b551587
al <allen.montz@gmail.com>
2016-07-29 15:38:11 -0500

    add some tests for fit

--- src/test/scala/DTreeTests.scala
+++ src/test/scala/DTreeTests.scala
@@ -36,4 +36,18 @@ class DTreeTests extends FunSuite {
     assert(mean(Vector(-1,0,1)) == 0)
     assert(mean(Vector(1,2,3)) == 2)
   }
+
+  test("fit with depth 0") {
+    assert(fit(Vector(Vector(0,0), Vector(1,0)), 0) == Leaf(0.0))
+    assert(fit(Vector(Vector(0,0), Vector(1,1)), 0) == Leaf(0.5))
+  }
+
+  test("fit") {
+    val d1 = Vector(1, 1, 1, 1)
+    val d2 = Vector(1, 1, 0, 1)
+    val d3 = Vector(0, 0, 1, 2)
+    val d4 = Vector(1, 0, 0, 2)
+    val d = Vector(d1, d2, d3, d4)
+    assert(fit(d) === Branch(Leaf(1.0), Leaf(2.0), (1, 1)))
+  }
 }
