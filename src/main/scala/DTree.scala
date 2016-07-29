5ffefc607de6247423a72730ba259dabf6df684b
al <allen.montz@gmail.com>
2016-07-29 09:27:35 -0500

    implement log2, entropy, infoGain, and best split on feature

--- src/main/scala/DTree.scala
+++ src/main/scala/DTree.scala
@@ -2,11 +2,44 @@ package randomForest
 
 import math.log
 
+// type split = (Int, Int) : (feature, value) = (index, level)
+
 sealed trait DTree[+A]
 case class Leaf[A](value: A) extends DTree[A]
-case class Branch[A](left: DTree[A], right: DTree[A]) extends DTree[A]
+case class Branch[A](left: DTree[A], right: DTree[A], split: (Int, Int)) extends DTree[A]
 
 object DTree {
 
-  def log2(x: Double) = log(x)/log(2)
+  def fit(data: Vector[Vector[Int]]): DTree[Double] = {
+    val split = getSplit(data)
+    Leaf(1.0) // remove
+  }
+
+  def getSplit(data: Vector[Vector[Int]]): (Int, Int) = {
+    (0,0)
+  }
+
+  def bestSplitFeature(v: Vector[Int]): (Int, Double) = {
+    v.distinct
+     .map(l => (l, infoGain(v, v.partition(a => a == l))))
+     .maxBy(_._2)
+  }
+
+  def infoGain(p: Vector[Int], c: (Vector[Int], Vector[Int])): Double = {
+    val n = p.length.toDouble
+
+    //TODO: no map for tuple, how to better handle this?
+    entropy(p) - c._1.length.toDouble/n*entropy(c._1) - c._2.length.toDouble/n*entropy(c._2)
+  }
+
+  def entropy(v: Vector[Int]): Double = {
+    v.groupBy(x => x)                             // returns a Map, i.e., tuple (val, Vector(...))
+     .mapValues(gv => gv.length.toDouble/v.length)
+     .mapValues(p => -p*log2(p))
+     .foldLeft(0.0)(_ + _._2)                     // left is double, right is "Map"
+                                                  // reverse order for foldRight
+                                                  // how to use fold?
+  }
+
+  def log2(x: Double): Double = log(x)/log(2)
 }
--- src/test/scala/DTreeTests.scala
+++ src/test/scala/DTreeTests.scala
@@ -12,4 +12,17 @@ class DTreeTests extends FunSuite {
     assert(log2(2.0) == 1)
     assert(abs(log2(10.0) - 3.3219280948873626) < math.pow(10, -15))
   }
+
+  test("entropy") {
+    assert(entropy(Vector(1,1,1)) == 0.0)
+    assert(entropy(Vector(0,1,0,1)) == 1.0)
+  }
+
+  test("info gain") {
+    assert(infoGain(Vector(0,1,0,1), (Vector(0,0), Vector(1,1))) == 1.0)
+  }
+
+  test("best split for a feature") {
+    assert(bestSplitFeature(Vector(0,0,1)) == (0, 0.9182958340544896))
+  }
 }
